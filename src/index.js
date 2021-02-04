cb8e8f91a7eed2b26ecffd133aa96809a342f8e9
김지운 <expect.ta@gmail.com>
2021-02-04 10:57:07 +0900

    [client] ADD : Login화면 컴포넌트 화면 구성관련 코드 작성.
    Login 컴포넌트에 대한 화면 구성관련 코드 작성함.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import LoginForm from "./compoments/LoginForm";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <LoginForm />
   </React.StrictMode>,
   document.getElementById("root"),
 );
