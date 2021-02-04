dc25ea7a14f403b987d2a94d36a0f5c57f9a44f2
southppp22 <tnddl032@gmail.com>
2021-02-04 10:50:58 +0900

    [client]ADD Nav component
    Nav컴포넌트 화면구성 스케치

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import Nav from "./components/Nav";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 function App() {
-  return <div className="App" />;
+  return (
+    <Router>
+      <GlobalStyle />
+      <Nav />
+      <Switch />
+    </Router>
+  );
 }
 
 export default App;
