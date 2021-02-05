cf3391fafaa7da71554d6eecb4c47e5d368a10e9
southppp22 <tnddl032@gmail.com>
2021-02-05 18:49:42 +0900

    [client]UPDATE star rating
    별점 기능 추가

--- src/compoments/Recipe.js
+++ src/compoments/Recipe.js
@@ -5,7 +5,6 @@ import PropTypes from "prop-types";
 export default function Recipe({ recipe }) {
   const { title, thumnailUrl, userName, rate, views } = recipe;
   const starRate = Number(rate) * 20;
-  console.log(starRate);
   return (
     <RecipeWrap>
       <Thumnail>
