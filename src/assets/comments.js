afa8591a6d734627881d24f2b769216d93cb1ce9
southppp22 <tnddl032@gmail.com>
2021-02-05 23:45:41 +0900

    [client]MODIFY css of CommentContainer

--- src/assets/comments.js
+++ src/assets/comments.js
@@ -3,7 +3,7 @@ const comments = [
     id: 1,
     nickname: "김코딩",
     createdAt: "2021-02-22",
-    text: "hi",
+    text: "만들기가 쉬워서 좋아요.",
     rate: "4.5",
   },
   {
@@ -17,7 +17,7 @@ const comments = [
     id: 3,
     nickname: "김코딩",
     createdAt: "2021-02-22",
-    text: "hi",
+    text: "test3",
     rate: "3.7",
   },
 ];
