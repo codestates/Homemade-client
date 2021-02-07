ab33a96243401e82d75e3ac5ce23512f5c1749b5
김지운 <expect.ta@gmail.com>
2021-02-07 18:23:51 +0900

    [client] UPDATE : SignUp 비밀번호 중복에 대한 로직개발
    유저가 회원 가입을 시도 할 경우 비밀번호 일치에 여부 개발

--- src/compoments/SignUpForm.js
+++ src/compoments/SignUpForm.js
@@ -1,7 +1,122 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
+export default function SignUpForm({ show, isShow }) {
+  if (!show) {
+    return null;
+  }
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [message, setMessage] = useState("");
+  const [isValidPassword, setIsValidPassword] = useState(false);
+
+  // 비밀번호 일치여부 판단
+  const handleConfirmPassword = event => {
+    const { value } = event.target;
+    setPassword(value);
+  };
+  const handleConfirmrePassword = event => {
+    const { value } = event.target;
+    setPasswordCheck(value);
+    if (value !== password) {
+      setMessage("비밀번호 불일치");
+      setIsValidPassword(false);
+    } else if (value === " ") {
+      setMessage("");
+    } else if (value === password) {
+      setMessage("비밀번호 일치");
+      setIsValidPassword(true);
+    }
+  };
+
+  useEffect(() => {});
+
+  return (
+    <DarkBackground>
+      <SignUpFormStyle>
+        <h3>회원가입</h3>
+        <p id="notification">모든항목은 필수입력 사항입니다.</p>
+        <form>
+          <table id="signup-form-table">
+            <tr>
+              <td className="label">이름</td>
+              <td className="input-tag">
+                <input type="text" name="name" required />
+              </td>
+            </tr>
+            <tr>
+              <td className="label">eamil</td>
+              <td className="input-tag">
+                <input type="text" name="eamil" required />
+              </td>
+              <td>
+                <Button id="overlapping-button">중복확인 </Button>
+              </td>
+            </tr>
+            <tr>
+              <td className="label">비밀번호</td>
+              <td className="input-tag">
+                <input
+                  type="password"
+                  name="firstPassword"
+                  placeholder="비밀번호를 입력하세요"
+                  value={password}
+                  onChange={handleConfirmPassword}
+                  required
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className="label">비밀번호 확인</td>
+              <td className="input-tag">
+                <input
+                  type="password"
+                  name="lastPassword"
+                  placeholder="비밀번호를 입력하세요"
+                  value={passwordCheck}
+                  onChange={handleConfirmrePassword}
+                  required
+                />
+              </td>
+              <td>
+                <Error check={isValidPassword}>{message}</Error>
+              </td>
+            </tr>
+            <tr>
+              <td className="label">닉네임</td>
+              <td className="input-tag">
+                <input type="text" name="password" required />
+              </td>
+            </tr>
+            <tr>
+              <td className="label">핸드폰 번호</td>
+              <td className="input-tag">
+                <input
+                  type="text"
+                  name="mobile"
+                  placeholder="'-' 는 제외한 숫자만 입력바랍니다"
+                  required
+                />
+              </td>
+            </tr>
+          </table>
+          <div id="button-wrap">
+            <input type="submit" id="sign-up" value="가입하기" />
+            <Button type="button" onClick={() => isShow(false)}>
+              취소
+            </Button>
+          </div>
+        </form>
+      </SignUpFormStyle>
+    </DarkBackground>
+  );
+}
+
+SignUpForm.propTypes = {
+  show: PropTypes.bool.isRequired,
+  isShow: PropTypes.func.isRequired,
+};
 // SignUpForm 주변영역
 const DarkBackground = styled.div`
   position: fixed;
@@ -17,7 +132,7 @@ const DarkBackground = styled.div`
 `;
 // SignUpForm 영역
 const SignUpFormStyle = styled.div`
-  width: 500px;
+  width: 700px;
   padding: 1.5rem;
   background: white;
   border-radius: 5px;
@@ -98,80 +213,8 @@ const Button = styled.button`
   color: white;
   border: 1px solid lightgray;
 `;
-
-export default function SignUpForm({ show, isShow }) {
-  if (!show) {
-    return null;
-  }
-  return (
-    <DarkBackground>
-      <SignUpFormStyle>
-        <h3>회원가입</h3>
-        <p id="notification">모든항목은 필수입력 사항입니다.</p>
-        <form>
-          <table id="signup-form-table">
-            <tr>
-              <td className="label">eamil</td>
-              <td className="input-tag">
-                <input type="text" name="eamil" required />
-              </td>
-              <td>
-                <Button id="overlapping-button">중복확인 </Button>
-              </td>
-            </tr>
-            <tr>
-              <td className="label">비밀번호</td>
-              <td className="input-tag">
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="비밀번호를 입력하세요"
-                  required
-                />
-              </td>
-            </tr>
-            <tr>
-              <td className="label">비밀번호 확인</td>
-              <td className="input-tag">
-                <input
-                  type="password"
-                  name="password-check"
-                  placeholder="비밀번호를 입력하세요"
-                  required
-                />
-              </td>
-            </tr>
-            <tr>
-              <td className="label">닉네임</td>
-              <td className="input-tag">
-                <input type="text" name="password" required />
-              </td>
-            </tr>
-            <tr>
-              <td className="label">핸드폰 번호</td>
-              <td className="input-tag">
-                <input
-                  type="text"
-                  name="mobile"
-                  placeholder="'-' 는 제외한 숫자만 입력바랍니다"
-                  required
-                />
-              </td>
-            </tr>
-          </table>
-          <div id="button-wrap">
-            <input type="submit" id="sign-up" value="가입하기" />
-            <Button type="button" onClick={() => isShow(false)}>
-              취소
-            </Button>
-          </div>
-        </form>
-      </SignUpFormStyle>
-    </DarkBackground>
-  );
-}
-
-SignUpForm.propTypes = {
-  show: PropTypes.bool.isRequired,
-  isShow: PropTypes.func.isRequired,
-};
+const Error = styled.span`
+  font-size: 0.7rem;
+  float: left;
+  color: ${props => (props.check ? "green" : "red")};
+`;
