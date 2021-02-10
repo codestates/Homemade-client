f2900fcc87517ce4229353da961a3058e9cf3e18
김지운 <expect.ta@gmail.com>
2021-02-11 02:36:45 +0900

    [client] UPDATE : UserInfo 로직 수정
    유저정보 컴포넌트 수정 관련 코드 수정

--- src/pages/UserInfo.js
+++ src/pages/UserInfo.js
@@ -1,6 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 import Information from "../compoments/Information";
+import UserRecipe from "../compoments/UserRecipe";
 
 export default function UserInfo() {
-  return <Information />;
+  const menuList = {
+    0: <Information />,
+    1: <UserRecipe />,
+  };
+  const [menu, setMenu] = useState(0);
+  // eslint-disable-next-line no-unused-vars
+  const [checked, setChecked] = useState(false);
+
+  // eslint-disable-next-line no-unused-vars
+  const handleMenu = menuIndex => {
+    setMenu(menuIndex);
+  };
+  useEffect(() => {
+    console.log(menu, " = 현재 번호");
+  });
+  return (
+    <>
+      <MenuContainer>
+        <MenuButton>
+          <Button
+            type="button"
+            id="userInfo"
+            checked={menu}
+            className={menu === 0 ? "active" : ""}
+            onClick={() => handleMenu(0)}
+          >
+            회원정보
+          </Button>
+          <Button
+            type="button"
+            id="myRecipe"
+            checked={menu}
+            className={menu === 1 ? "active" : ""}
+            onClick={() => handleMenu(1)}
+          >
+            내 레시피
+          </Button>
+        </MenuButton>
+        <ViewComponent>{menuList[menu]}</ViewComponent>
+      </MenuContainer>
+    </>
+  );
 }
+const MenuContainer = styled.div`
+  border: 1px solid lightgray;
+  border-radius: 0px 0px 10px 10px;
+  background: lightgray;
+  .active {
+    background: white;
+  }
+`;
+const MenuButton = styled.div``;
+
+const ViewComponent = styled.div`
+  background: white;
+  border-radius: 0px 0px 10px 10px;
+`;
+const Button = styled.button`
+  font-weight: bold;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  border: 1px solid white;
+  margin-top: 5px;
+  margin-right: 8px;
+  border-radius: 10px 10px 0px 0px;
+  display: inline-block;
+  height: 40px;
+  border: 1px solid white;
+`;
