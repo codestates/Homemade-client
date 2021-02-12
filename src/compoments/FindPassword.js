d2863bca0dc7a5b4755052d0848da0afeb2d7453
김지운 <expect.ta@gmail.com>
2021-02-12 19:19:27 +0900

    [client] UPDATE : 비밀번호 변경 API 연결 및 코드 수정
    API 연결 및 예외처리 코드 수정

--- src/compoments/FindPassword.js
+++ src/compoments/FindPassword.js
@@ -1,23 +1,25 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-// import axios from "axios";
+import { BiExit } from "react-icons/bi";
+import axios from "axios";
+import NorificationModal from "./NotificationModal";
 
 export default function FindPassword({ show, isShow, setIsFindPassword }) {
   // 비밀번호 찾기 상태
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [mobile, setMobile] = useState("");
-  // eslint-disable-next-line no-unused-vars
-  const [temporarilyEmail, seTemporarilyEmail] = useState("");
   // 비밀번호 변경에 관한 상태
-  // TODO : 회원정보의 여부에 따라 passwordChangeState를 true로 변경 필요.
   // eslint-disable-next-line no-unused-vars
   const [passwordChangeState, setPasswordChangeState] = useState(false);
   const [firstPassword, setFirstPassword] = useState("");
   const [lastPassword, setLastPassword] = useState("");
   const [message, setMessage] = useState("");
   const [isValidPassword, setIsValidPassword] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
   // 유저이름 input 핸들링
   const hadleName = event => {
     setName(event.target.value);
@@ -29,11 +31,24 @@ export default function FindPassword({ show, isShow, setIsFindPassword }) {
   // password input 핸들링
   const handleMobile = event => {
     setMobile(event.target.value);
+    console.log(name, " 이름", email, " 메일 ", mobile, " 모바일");
   };
   // 닫기버튼, 취소 버튼을 눌렀을 시 핸들링
   const handleCancel = () => {
     setIsFindPassword(false);
     isShow(false);
+  };
+  // modal창 종료
+  const closeModal = () => {
+    setModalVisible(false);
+    handleCancel();
+  };
+  // modal창 오픈 시
+  const openModal = buttonName => {
+    if (buttonName === "changePassowrd") {
+      setModalMessage("비밀번호가 변경 되었습니다");
+    }
+    setModalVisible(true);
   };
   // 비밀번호 일치여부 판단
   //! 비밀번호 , 비밀번호 확인 input 태그에 똑같은 조건이 모두 있어야 함.
@@ -56,45 +71,18 @@ export default function FindPassword({ show, isShow, setIsFindPassword }) {
     const { value } = event.target;
     setLastPassword(value);
     if (value !== firstPassword) {
-      setMessage("비밀번호 불일치");
       setIsValidPassword(false);
+      setMessage("비밀번호 불일치");
     } else if (value === " ") {
       setMessage("");
     } else if (value === firstPassword) {
-      setMessage("비밀번호 일치");
       setIsValidPassword(true);
+      setMessage("비밀번호 일치");
     }
   };
-  // 유저의 정보유무확인을 위한 서버통신
-  //! 인증되지 않은 회원이 회원정보를 요청하기 떄문에 토큰이 없음
-  //! 서버 API 작성 후 테스트 필요
-  const findReuestPassword = () => {
-    if (name && email && mobile) {
-      console.log("정상입력");
-      try {
-        // axios
-        //   .post("https://homemade2021.ml/users/", {
-        //     name,
-        //     email,
-        //     mobile,
-        //   })
-        //   .then(() => {
-        setMessage("");
-        seTemporarilyEmail(email);
-        setPasswordChangeState(true);
-        alert("응답");
-        return;
-        // });
-      } catch (err) {
-        alert(err);
-        //   setMessage("회원정보를 찾지 못했습니다.");
-        //   setIsValidPassword(false);
-      }
-    }
-    setMessage("필수 입력사항을 모두입력하세요");
-    setIsValidPassword(false);
-  };
+
   // 회원의 비밀번호를 변경요청
+  // 이메일과 패스워드만
   const changeRequestPassword = () => {
     if (firstPassword.length === 0 || lastPassword.length === 0) {
       setMessage("모든 입력사항은 필수입니다.");
@@ -102,35 +90,60 @@ export default function FindPassword({ show, isShow, setIsFindPassword }) {
       return;
     }
     if (firstPassword === lastPassword) {
-      console.log("정상입력");
       try {
-        // axios
-        //   .post("https://homemade2021.ml/users/", {
-        //    email: temporarilyEmail,
-        // password : lastPassword
-        //   })
-        //   .then(() => {
-        seTemporarilyEmail("");
-        setPasswordChangeState(false);
-        handleCancel(false);
-        alert("응답");
-        return;
-        // });
+        axios
+          .patch("https://homemade2021.ml/users/upassword", {
+            email,
+            password: lastPassword,
+          })
+          .then(() => {
+            setModalVisible(true);
+            openModal("changePassowrd");
+          })
+          .catch(err => {
+            console.log(err);
+          });
       } catch (err) {
         alert(err);
-        //   setMessage("회원정보를 찾지 못했습니다.");
-        //   setIsValidPassword(false);
+        setMessage("회원정보를 찾지 못했습니다.");
+        setIsValidPassword(false);
       }
     }
-    setMessage("비밀번호가 일치하지 않습니다.");
-    setIsValidPassword(false);
+  };
+
+  // 유저의 정보유무확인을 위한 서버통신
+  //! 인증되지 않은 회원이 회원정보를 요청하기 떄문에 토큰이 없음
+  //! 서버 API 작성 후 테스트 필요
+  const findReuestPassword = async () => {
+    console.log("시작");
+    console.log("시작");
+    // if (name && email && mobile) {
+    try {
+      await axios
+        .patch("https://homemade2021.ml/users/upassword", {
+          name,
+          email,
+          mobile,
+        })
+        .then(() => {
+          setPasswordChangeState(true);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    } catch (err) {
+      alert(err);
+    }
   };
   if (!show) {
+    alert("언제나오냐");
     return null;
   }
   return (
     <LoginFormStyle>
-      <Close onClick={handleCancel}>닫기</Close>
+      <Close onClick={handleCancel}>
+        <BiExit size="24" />
+      </Close>
       {passwordChangeState ? (
         <div>
           <h3>비밀번호 변경</h3>
@@ -154,46 +167,54 @@ export default function FindPassword({ show, isShow, setIsFindPassword }) {
               />
               <Error check={isValidPassword}>{message}</Error>
             </InputWrap>
-            <Button onClick={changeRequestPassword}>변경</Button>
+            <Button name="changePassowrd" onClick={changeRequestPassword}>
+              변경
+            </Button>
             <Button onClick={handleCancel}>취소</Button>
           </div>
         </div>
       ) : (
         <div>
           <h3>비밀번호 찾기</h3>
-          <form>
-            <InputWrap>
-              <input
-                type="text"
-                name="eamil"
-                value={name}
-                placeholder="성함을 입력하세요"
-                onChange={hadleName}
-                required
-              />
-              <input
-                type="text"
-                name="eamil"
-                value={email}
-                placeholder="email 을 입력하세요"
-                onChange={handleEmail}
-                required
-              />
-              <input
-                type="text"
-                name="mobile"
-                value={mobile}
-                placeholder="핸드폰 번호를 입력하세요. '-' 제외"
-                onChange={handleMobile}
-                required
-              />
-              <Error check={isValidPassword}>{message}</Error>
-            </InputWrap>
-            <Button onClick={findReuestPassword}>찾기</Button>
-            <Button onClick={handleCancel}>취소</Button>
-          </form>
+          <InputWrap>
+            <input
+              type="text"
+              name="name"
+              value={name}
+              placeholder="성함을 입력하세요"
+              onChange={hadleName}
+              required
+            />
+            <input
+              type="text"
+              name="eamil"
+              value={email}
+              placeholder="email 을 입력하세요"
+              onChange={handleEmail}
+              required
+            />
+            <input
+              type="text"
+              name="mobile"
+              value={mobile}
+              placeholder="핸드폰 번호를 입력하세요. '-' 제외"
+              onChange={handleMobile}
+              required
+            />
+            <Error check={isValidPassword}>{message}</Error>
+          </InputWrap>
+          <Button onClick={findReuestPassword}>찾기</Button>
+          <Button onClick={handleCancel}>취소</Button>
         </div>
       )}
+      <NorificationModal
+        visible={modalVisible}
+        closeable
+        maskClosable
+        onClose={closeModal}
+      >
+        <h3>{modalMessage}</h3>
+      </NorificationModal>
     </LoginFormStyle>
   );
 }
