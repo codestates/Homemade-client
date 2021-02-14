9498a94e4b22133f33ae989fc6924306393f00fc
southppp22 <tnddl032@gmail.com>
2021-02-14 21:24:16 +0900

    [client]UPDATE Nav Component
    Nav바 디자인 수정

--- src/compoments/Nav.js
+++ src/compoments/Nav.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { darken, lighten } from "polished";
-import { MdMenu } from "react-icons/md";
-import { FiUser, FiSearch } from "react-icons/fi";
+import { AiOutlineSearch, AiOutlineMenu, AiOutlineUser } from "react-icons/ai";
 import { HiOutlinePencil } from "react-icons/hi";
+import { nanoid } from "nanoid";
 import LoginForm from "./LoginForm";
 import SignUpForm from "./SignUpForm";
 
-export default function Nav({ isLogin, signInHanlder, handleLogOut }) {
+export default function Test({ isLogin, signInHanlder, handleLogOut }) {
   const [showLogin, setShowLogin] = useState(false);
   const [showSignup, setShowSignUp] = useState(false);
   const [keyword, setKeyword] = useState("");
@@ -17,59 +17,70 @@ export default function Nav({ isLogin, signInHanlder, handleLogOut }) {
   const categories = ["한식", "중식", "양식", "일식", "음료/술"];
 
   return (
-    <NavWrap>
+    <>
       <TopBar>
-        <Info>
-          {isLogin ? (
-            <InfoBtn onClick={handleLogOut}>로그아웃</InfoBtn>
-          ) : (
-            <>
-              <InfoBtn onClick={() => setShowSignUp(true)}>회원가입</InfoBtn>
-              <InfoBtn onClick={() => setShowLogin(true)}>로그인</InfoBtn>
-            </>
-          )}
-        </Info>
+        <LogoWrapper to="/">
+          <Logo src="../images/logo.png" />
+        </LogoWrapper>
+        <MenuContainer>
+          <MenuWrapper>
+            <LeftDiv />
+            <CenterDiv>
+              <CateogryButtonDiv>
+                <CategoryButton />
+                <CategoryHover />
+                <CategoryItemDiv>
+                  {categories.map((category, idx) => (
+                    <CategoryItem
+                      key={nanoid()}
+                      to={`/search?category=${categories[idx]}&page=1`}
+                    >
+                      {category}
+                    </CategoryItem>
+                  ))}
+                </CategoryItemDiv>
+              </CateogryButtonDiv>
+              <SearchDiv>
+                <Search
+                  name="keyword"
+                  value={keyword}
+                  onChange={e => setKeyword(e.target.value)}
+                  type="text"
+                  placeholder="찾고 싶은 레시피를 검색해 보세요!"
+                />
+                <SearchLink to={`/search?q=${keyword}&page=1`}>
+                  <SearchButton />
+                </SearchLink>
+              </SearchDiv>
+              <MyPageLink to="/userinfo">
+                <MyPage />
+              </MyPageLink>
+              <WriteLink to="/postrecipe">
+                <Write />
+              </WriteLink>
+            </CenterDiv>
+            <RightDiv />
+          </MenuWrapper>
+        </MenuContainer>
       </TopBar>
       <BottomBar>
-        <Category>
-          <Logo to="/">Homemade</Logo>
-          <CategoryMenu>
-            <MdMenu />
-            <span>카테고리</span>
-            <DropDownMenu>
-              {categories.map((category, idx) => (
-                <DropDownContent
-                  key={category}
-                  to={`/search?category=${categories[idx]}&page=1`}
-                >
-                  {category}
-                </DropDownContent>
-              ))}
-            </DropDownMenu>
-          </CategoryMenu>
-          <CategoryItem>랭킹</CategoryItem>
-          <CategoryInput>
-            <Search
-              name="keyword"
-              value={keyword}
-              onChange={e => setKeyword(e.target.value)}
-              type="text"
-            />
-            <SearchRecipe to={`/search?q=${keyword}&page=1`}>
-              <FiSearch />
-            </SearchRecipe>
-          </CategoryInput>
-          <CategoryItem>
-            <MyPageLink to="/userinfo">
-              <FiUser />
-            </MyPageLink>
-          </CategoryItem>
-          <CategoryItem>
-            <MyPageLink to="/postrecipe">
-              <HiOutlinePencil />
-            </MyPageLink>
-          </CategoryItem>
-        </Category>
+        <LeftDiv bottom />
+        <AccessDiv>
+          {isLogin ? (
+            <AccessBtn type="button" onClick={() => handleLogOut()}>
+              로그아웃
+            </AccessBtn>
+          ) : (
+            <>
+              <AccessBtn type="button" onClick={() => setShowLogin(true)}>
+                로그인
+              </AccessBtn>
+              <AccessBtn type="button" onClick={() => setShowSignUp(true)}>
+                회원가입
+              </AccessBtn>
+            </>
+          )}
+        </AccessDiv>
       </BottomBar>
       <LoginForm
         show={showLogin}
@@ -77,159 +88,114 @@ export default function Nav({ isLogin, signInHanlder, handleLogOut }) {
         signInHanlder={signInHanlder}
       />
       <SignUpForm show={showSignup} isShow={setShowSignUp} />
-    </NavWrap>
+    </>
   );
 }
 
-Nav.propTypes = {
-  isLogin: PropTypes.bool.isRequired,
-  signInHanlder: PropTypes.func.isRequired,
-  handleLogOut: PropTypes.func.isRequired,
-};
-
-const NavWrap = styled.header`
-  width: 100%
-  height: 6rem;
-`;
-const TopBar = styled.article`
-  width: 100%;
-  height: 2rem;
-  background-color: #f0f0f0;
-`;
-const Info = styled.section`
-  width: 1020px;
-  height: 2rem;
-  margin: 0 auto;
+const AccessDiv = styled.div`
   display: flex;
+  height: 100%;
   align-items: center;
-  flex-direction: row-reverse;
-  @media (max-width: 1020px) {
-    width: 80%;
-  }
+  flex: 5;
 `;
-const InfoBtn = styled.button`
-  color: #6f6f6f;
-  font-size: 0.8rem;
-  margin-left: 1rem;
-  text-decoration: none;
+
+const AccessBtn = styled.button`
   border: none;
+  background: #ffffff;
+  font-size: 1.1rem;
+  font-weight: 600;
   cursor: pointer;
+  padding-left: 2rem;
+
+  &:focus {
+    outline: none;
+  }
   &:hover {
     color: ${lighten(0.1, `#6f6f6f`)};
   }
   &:active {
     color: ${darken(0.1, `#6f6f6f`)};
   }
+`;
+
+const TopBar = styled.div`
+  position: relative;
+  z-index: 99;
+  width: 100%;
+  height: 14vh;
+  font-size: 2rem;
+  display: flex;
+`;
+
+const MenuContainer = styled.div`
+  width: 100%;
+  flex: 100;
+  display: flex;
+  flex-direction: column;
+`;
+
+const MenuWrapper = styled.div`
+  display: flex;
+  flex: 1;
+`;
+
+const LeftDiv = styled.div`
+  flex: 3;
+
+  ${({ bottom }) =>
+    bottom &&
+    `
+    flex: 9.8;
+  `}
+`;
+
+const RightDiv = styled.div`
+  flex: 4;
+`;
+
+const CenterDiv = styled.div`
+  flex: 10;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background: #ffffff;
+`;
+
+const SearchDiv = styled.div`
+  position: relative;
+  height: 40%;
+  width: 65%;
+  text-align: left;
+  background: #fafafa;
+  border: 1px solid #cccccc;
+  border-radius: 12px;
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+`;
+
+const Search = styled.input`
+  height: 100%;
+  width: 85%;
+  background: #fafafa;
+  border: none;
+  padding: 0 1rem;
+  font-size: 1.3rem;
+
   &:focus {
     outline: none;
   }
 `;
-const BottomBar = styled.article`
-  svg {
-    font-size: 2rem;
-  }
-  width: 100%;
-  height: 4rem;
-  background-color: #9a8b80;
+
+const SearchLink = styled(Link)`
+  height: 100px;
+  color: #000000;
 `;
-const Category = styled.section`
-  width: 1020px;
-  height: 100%;
-  margin: 0 auto;
-  display: table;
-  color: white;
-  font-size: 1.1rem;
-`;
-const CategoryItem = styled.div`
-  max-width: 168px;
-  cursor: pointer;
-  display: table-cell;
-  line-height: 100%;
-  vertical-align: middle;
-  flex: 2;
-  max-width: 100px;
-  margin-left: 10px;
-  &:hover {
-    color: ${lighten(0.4, `#6f6f6f`)};
-  }
-  &:active {
-    color: ${darken(0.1, `#6f6f6f`)};
-  }
-`;
-const Logo = styled(CategoryItem.withComponent(Link))`
-  color: white;
-  text-decoration: none;
-  width: 168px;
-  font-size: 1.3rem;
-`;
-const MyPageLink = styled(Link)`
-  color: white;
-  &:hover {
-    color: ${lighten(0.4, `#6f6f6f`)};
-  }
-  &:active {
-    color: ${darken(0.1, `#6f6f6f`)};
-  }
-`;
-const SearchRecipe = styled(Link)`
-  &:active {
-    color: ${darken(0.1, `#6f6f6f`)};
-  }
-`;
-const CategoryMenu = styled(CategoryItem)`
-  width: 168px;
-  span {
-    position: relative;
-    top: -9px;
-  }
-  svg {
-    margin-right: 8px;
-  }
-  &:hover {
-    color: white;
-    div {
-      display: block;
-    }
-  }
-`;
-const CategoryInput = styled.span`
-  position: relative;
-  flex: 4;
-  display: flex;
-  width: 100%;
-  svg {
-    font-size: 1.6rem;
-    position: absolute;
-    top: 18px;
-    right: 50px;
-    color: black;
-    &:active {
-      color: ${darken(0.1, `#6f6f6f`)};
-    }
-  }
-`;
-const Search = styled.input`
-  outline: none;
-  width: 90%;
-  height: 36px;
-  border-radius: 8px;
-  border: 0.5px solid #6f6f6f;
-  margin-top: 14px;
-`;
-const DropDownMenu = styled.div`
-  display: none;
+
+const SearchButton = styled(AiOutlineSearch)`
   position: absolute;
-  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-  background-color: #f1f1f1;
-  width: 120px;
-  z-index: 1;
-  transform: translateY(1rem);
-`;
-const DropDownContent = styled(Link)`
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
+  top: 50%;
+  right: 0;
+  transform: translate(-25%, -50%);
+  padding: 0.3rem;
   &:hover {
     color: ${lighten(0.1, `#6f6f6f`)};
   }
@@ -237,3 +203,125 @@ const DropDownContent = styled(Link)`
     color: ${darken(0.1, `#6f6f6f`)};
   }
 `;
+
+const CateogryButtonDiv = styled.div`
+  position: relative;
+  height: 52px;
+  width: 52px;
+  text-align: center;
+  vertical-align: middle;
+  border: 1px solid #dddddd;
+  border-radius: 50%;
+  &:hover {
+    div {
+      display: block;
+      position: relative;
+    }
+  }
+`;
+
+const CategoryButton = styled(AiOutlineMenu)`
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  &:hover {
+    color: #ff4c4c;
+  }
+`;
+
+const CategoryItemDiv = styled.div`
+  display: none;
+  top: 30px;
+  width: 160px;
+  border: 1px solid #cccccc;
+  border-radius: 12px;
+  background-color: #fafafa;
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+`;
+
+const CategoryItem = styled(Link)`
+  padding: 12px 16px;
+  font-size: 1.3rem;
+  text-decoration: none;
+  display: block;
+  color: #000000;
+  border-bottom: 1px solid #eeeeee;
+  &:hover {
+    color: ${lighten(0.1, `#6f6f6f`)};
+  }
+  &:active {
+    color: ${darken(0.1, `#6f6f6f`)};
+  }
+`;
+
+const CategoryHover = styled.div`
+  position: absolute;
+  top: 50px;
+  height: 30px;
+  width: 160px;
+`;
+
+const MyPageLink = styled(Link)`
+  position: relative;
+  height: 52px;
+  width: 52px;
+  text-align: center;
+  vertical-align: middle;
+  border: 1px solid #dddddd;
+  border-radius: 50%;
+`;
+
+const MyPage = styled(AiOutlineUser)`
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  color: #000000;
+  &:hover {
+    color: #ff4c4c;
+  }
+`;
+
+const WriteLink = styled(Link)`
+  position: relative;
+  height: 52px;
+  width: 52px;
+  text-align: center;
+  vertical-align: middle;
+  border: 1px solid #dddddd;
+  border-radius: 50%;
+  color: #000000;
+`;
+
+const Write = styled(HiOutlinePencil)`
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  &:hover {
+    color: #ff4c4c;
+  }
+`;
+
+const LogoWrapper = styled(Link)`
+  flex: 0.1;
+  width: 200px;
+`;
+
+const Logo = styled.img`
+  height: 100%;
+  object-fit: cover;
+`;
+
+const BottomBar = styled.div`
+  display: flex;
+  width: 100%;
+  height: 6vh;
+  border-top: 0.1px solid #f1f1f1;
+  border-bottom: 0.5px solid #f1f1f1;
+`;
+
+Test.propTypes = {
+  isLogin: PropTypes.bool.isRequired,
+  signInHanlder: PropTypes.func.isRequired,
+  handleLogOut: PropTypes.func.isRequired,
+};
